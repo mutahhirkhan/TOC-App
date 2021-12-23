9ebc5dc5d2853cdd92dac361d1c383a991b5285e
Muhammad Arbab Hussain Siddiqui <muhammad.arbab.hussain.siddiqui@gmail.com>
2021-12-23 19:09:15 +0500

    useless commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,7 +5,9 @@ import "./Home.css"
 const Home = () => {
     return (
         <div className="home">
-                <div className="homeLeft center">LEFT</div>
+                <div className="homeLeft center">
+                    <h1>Image 1</h1>
+                </div>
                 <div className="homeRight center">RIGHT</div>
         </div>
     )
