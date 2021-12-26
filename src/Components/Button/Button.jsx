d0cff6e12c75adaf9d30a44fe2d162abd636a07a
Mutahhir Khan <mutahhir199@gmail.com>
2021-12-26 13:17:40 +0500

    product cardV2 nad buttons and heading modified

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -3,37 +3,37 @@ import "./Button.css";
 import Paragraph from "./../Paragraph/Paragraph";
 
 const Button = ({
-  children,
-  background = "#0E5E6D",
-  color = "white",
-  fontWeight,
-  fontSize,
-  style = {},
-  disabled = false,
-  ...restProps
+    children,
+    background = "#0E5E6D",
+    color = "white",
+    fontWeight,
+    fontSize,
+    classname,
+    style = {},
+    disabled = false,
+    ...restProps
 }) => {
-  // console.log(disabled);c
-  return (
-    <button
-      {...restProps}
-      className="button"
-      disabled={disabled}
-      style={{
-        background: disabled ? "lightgrey" : background,
-        color,
-        ...style,
-      }}
-    >
-      <Paragraph
-        style={{ textDecoration: disabled ? "line-through" : "" }}
-        color={disabled ? "black" : color}
-        fontSize={fontSize}
-        fontWeight={fontWeight}
-      >
-        {children}
-      </Paragraph>
-    </button>
-  );
+    console.log("style");
+    console.log(style);
+    return (
+        <button
+            {...restProps}
+            className={`${classname} button`}
+            disabled={disabled}
+            style={{
+                background: disabled ? "lightgrey" : background,
+                color,
+                ...style,
+            }}>
+            <Paragraph
+                style={{ textDecoration: disabled ? "line-through" : "" }}
+                color={disabled ? "black" : color}
+                fontSize={fontSize}
+                fontWeight={fontWeight}>
+                {children}
+            </Paragraph>
+        </button>
+    );
 };
 
 export default Button;
--- src/Pages/Test/Test.jsx
+++ src/Pages/Test/Test.jsx
@@ -7,34 +7,36 @@ import Header from "./../../Components/Header/Header";
 import Paragraph from "./../../Components/Paragraph/Paragraph";
 import Button from "./../../Components/Button/Button";
 import ProductCard from "./../../Components/ProductCard/ProductCard";
+import ProductCardV2 from "./../../Components/ProductCardV2/ProductCardV2";
 import { openModal } from "./../../Redux/modal/modalActions";
 
 const Test = ({ uploadProduct, uploadImages, openModal }) => {
-  // var [category, setcategory] = useState("")
-  // var [title, settitle] = useState("")
-  // var [cost, setcost] = useState("")
-  // var [description, setdescription] = useState("")
-  // var [quantity, setquantity] = useState("")
-  // var [CoverPhoto, setCoverPhoto] = useState(null)
+    // var [category, setcategory] = useState("")
+    // var [title, settitle] = useState("")
+    // var [cost, setcost] = useState("")
+    // var [description, setdescription] = useState("")
+    // var [quantity, setquantity] = useState("")
+    // var [CoverPhoto, setCoverPhoto] = useState(null)
 
-  // var handleSubmit = (e) => {
-  //     e.preventDefault()
-  //     var productObj = {
-  //         category,
-  //         title,
-  //         cost,
-  //         description,
-  //         quantity,
-  //         CoverPhoto,
-  //     }
-  //         // uploadImages(productObj)
-  //     uploadProduct(productObj)
-  // }
-  return (
-    /* 1em = 10px  */
-    <div style={{ fontSize: "62.5%" }}>
-      <h1>Test page</h1>
-      {/* <form onSubmit={handleSubmit}>
+    // var handleSubmit = (e) => {
+    //     e.preventDefault()
+    //     var productObj = {
+    //         category,
+    //         title,
+    //         cost,
+    //         description,
+    //         quantity,
+    //         CoverPhoto,
+    //     }
+    //         // uploadImages(productObj)
+    //     uploadProduct(productObj)
+    // }
+    return (
+        /* 1em = 10px  */
+        <div style={{ fontSize: "62.5%" }}>
+            <h1>Test page</h1>
+            {/* <ProductCardV2/> */}
+            {/* <form onSubmit={handleSubmit}>
                 <input onChange={(e) => setcategory(e.target.value)} type="text" value={category} placeholder="category"/> <br/>
                 <input onChange={(e) => settitle(e.target.value)} type="text" value={title} placeholder="title"/><br/>
                 <textarea onChange={(e) => setdescription(e.target.value)}  value={description} cols="30" rows="10" placeholder="Description"></textarea><br/>
@@ -43,9 +45,9 @@ const Test = ({ uploadProduct, uploadImages, openModal }) => {
                 <input onChange={(e) => setCoverPhoto(e.target.files[0])}  type="file"  placeholder="cover photo"/>
                 <button>ADD PRODUCT</button>
             </form> */}
-      {/* ------------------------------------------------------------------- */}
-      {/* <Cart/> */}
-      {/* <Header fontWeight="bold" fontSize={32}>This is  my Header</Header>
+            {/* ------------------------------------------------------------------- */}
+            {/* <Cart/> */}
+            {/* <Header fontWeight="bold" fontSize={32}>This is  my Header</Header>
             <Header fontWeight="regular" fontSize={16}>This is  my 2nd  Header</Header>
             <Header fontWeight="regular" fontSize={16} color="darkgreen">This is  my 2nd  Header</Header>
             
@@ -55,34 +57,18 @@ const Test = ({ uploadProduct, uploadImages, openModal }) => {
 
             <Button  fontWeight="semi-bold" >Click Me! </Button> */}
 
-      {/* <ProductCard /> */}
-      <Button
-        onClick={() =>
-          openModal({ modalType: "testModal", modalProps: { number: 2 } })
-        }
-      >
-        open test modal
-      </Button>
-      <Button
-        onClick={() =>
-          openModal({ modalType: "errorModal", modalProps: {error: "ops! </br> something went wrong "} })
-        }
-      >
-        open error modal
-      </Button>
-      <Button
-        onClick={() =>
-          openModal({ modalType: "addressFormModal"  })
-        }
-      >
-        open Address modal
-      </Button>
-    </div>
-  );
+            {/* <ProductCard /> */}
+            <Button onClick={() => openModal({ modalType: "testModal", modalProps: { number: 2 } })}>open test modal</Button>
+            <Button onClick={() => openModal({ modalType: "errorModal", modalProps: { error: "ops! </br> something went wrong " } })}>
+                open error modal
+            </Button>
+            <Button onClick={() => openModal({ modalType: "addressFormModal" })}>open Address modal</Button>
+        </div>
+    );
 };
 var actions = {
-  uploadProduct,
-  uploadImages,
-  openModal,
+    uploadProduct,
+    uploadImages,
+    openModal,
 };
 export default connect(null, actions)(Test);
